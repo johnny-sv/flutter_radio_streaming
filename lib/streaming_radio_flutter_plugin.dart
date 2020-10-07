c3f06e4636771a0e0bf0cc54ba7fc4edd8808429
Sinh Vu <sinh.vu@sotatek.com>
2020-10-08 01:16:24 +0700

    fix invokeMethod not found

--- lib/streaming_radio_flutter_plugin.dart
+++ lib/streaming_radio_flutter_plugin.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class Streaming {
-  static const MethodChannel _channel = const MethodChannel('streaming');
+  static const MethodChannel _channel = const MethodChannel('streaming_channel');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
