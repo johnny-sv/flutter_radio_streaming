56b2564db672e416e9b9849892dd30ad88ad039b
Sinh Vu <sinh.vu@sotatek.com>
2020-10-08 01:24:05 +0700

    fix invokeMethod method agrs

--- lib/streaming_radio_flutter_plugin.dart
+++ lib/streaming_radio_flutter_plugin.dart
@@ -6,12 +6,12 @@ class Streaming {
   static const MethodChannel _channel = const MethodChannel('streaming_channel');
 
   static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
+    final String version = await _channel.invokeMethod('getPlatformVersion', {});
     return version;
   }
 
   static Future<String> get getCurrentSong async {
-    final String title = await _channel.invokeMethod('getCurrentSong');
+    final String title = await _channel.invokeMethod('getCurrentSong', {});
     return title;
   }
 }
