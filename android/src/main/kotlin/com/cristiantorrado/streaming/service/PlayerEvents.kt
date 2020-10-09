ae5fff1ce95e290b8db0085818ae2cc4099b8105
Sinh Vu <sinh.vu@sotatek.com>
2020-10-09 11:28:32 +0700

    add missing events

--- android/src/main/kotlin/com/cristiantorrado/streaming/service/PlayerEvents.kt
+++ android/src/main/kotlin/com/cristiantorrado/streaming/service/PlayerEvents.kt
@@ -1,8 +1,12 @@
 package com.cristiantorrado.streaming.service
 
-open class PlayerEvent;
+open class PlayerEvent {
+    open var value: String = ""
+}
+
 class PlayingEvent: PlayerEvent()
 class StoppedEvent: PlayerEvent()
 class LoadingEvent: PlayerEvent()
 class PausedEvent: PlayerEvent()
 class ErrorEvent: PlayerEvent()
+class SongTitleUpdateEvent: PlayerEvent()
